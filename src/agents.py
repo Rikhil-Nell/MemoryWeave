5fba20a9c307e562dc795d112a973ab754871c01
Abhiram <abhiramvsa7@gmail.com>
2025-04-18 21:39:19 +0530

    agents.py updated

--- src/agents.py
+++ src/agents.py
@@ -1,16 +1,15 @@
-from dataclasses import dataclass
+
 from typing import List
-from pydantic_ai import Agent, RunContext
+from pydantic_ai import Agent
 from pydantic_ai.models.openai import OpenAIModelSettings, OpenAIModel
 from pydantic_ai.providers.openai import OpenAIProvider
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 import openai
-import questionary
-from supabase import create_async_client, AsyncClient
 import asyncio
 from load_api import Settings
-from questionary import text
 
+# from supabase import create_async_client, AsyncClient
+# from dataclasses import dataclass
 
 settings = Settings()
 
@@ -21,16 +20,21 @@ cerebras_openai_settings = OpenAIModelSettings(
     frequency_penalty=0,
 )
 
+# Model Name
 cerebras_model_name = "llama-3.3-70b"
 
+# Setting Client Up
 cerebras_client = openai.AsyncOpenAI(api_key=settings.CEREBRAS_API_KEY, base_url="https://api.cerebras.ai/v1")
 
+
+# Cerebras Model
 cerebras_model = OpenAIModel(
     model_name=cerebras_model_name,
     provider=OpenAIProvider(openai_client=cerebras_client),
 )
 
 
+# Validation Models
 class Memory(BaseModel):
     date: str
     objects_seen: List[str]
@@ -40,15 +44,20 @@ class MemoryContext(BaseModel):
     memories: List[Memory]
 
 
+# Needs to be updated through YOLO IMG Detection
 memories = [
     Memory(date="2025-04-01", objects_seen=["cat", "sofa", "sunlight"], summary="A cozy afternoon with a cat lounging."),
     Memory(date="2025-04-10", objects_seen=["books", "coffee"], summary="Reading session in the morning."),
 ]
 
+
+# Converting to a string
 memories_text = "\n".join(
     f"On {m.date}, you saw {', '.join(m.objects_seen)}. Summary: {m.summary}" for m in memories
 )
 
+
+# System Prompt
 system_prompt = f"""
 You need to create a journal like story for each object detected at an event. Here's what the user has experienced recently:
 {memories_text}
@@ -56,8 +65,7 @@ You need to create a journal like story for each object detected at an event. He
 Use this information to personalize your replies.
 """
 
-# Now pass system_prompt to your agent setup
-
+# Setting up Agent
 agent = Agent(
     model = cerebras_model,
     model_settings=cerebras_openai_settings,
@@ -65,18 +73,17 @@ agent = Agent(
     retries=2,
 )
 
-
+# Testing the Agent's Function
 user_input = 'Provide the journal'
 response = asyncio.run(agent.run(user_input))
 print(response.data)
 
 
+class Return(BaseModel):
+    pass
 
 
-
-# class Return(BaseModel):
-#     pass
-
+# Dataclass for DB AGENT
 # @dataclass
 # class Deps:
 #     supabase_url: str = settings.supabase_url
@@ -99,6 +106,7 @@ print(response.data)
 #     retries=3,
 #     result_type=Return
 # )
+
 
 # call_log_agent = Agent(
 #     model=cerebras_model,
