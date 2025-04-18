ff95b29bdd765673b508ed8da1aacaefaa553509
YuanziX <achubadyal4@gmail.com>
2025-04-18 14:34:10 +0530

    feat: push video details to database

--- app/api/v1/video.py
+++ app/api/v1/video.py
@@ -5,21 +5,26 @@ from gotrue.types import User
 
 from app.core.supabase import supabase
 from app.deps.auth import get_current_user
-from app.core.constants import UPLOAD_DIR
+from app.core.constants import UPLOAD_DIR, PROJECT_PATH
 
 video_router = APIRouter()
 
 
 @video_router.post("/upload")
 async def upload_video(file: UploadFile = File(...), user: User = Depends(get_current_user)):
-    if not file.content_type.startswith("video/"):
-        raise HTTPException(status_code=400, detail="Invalid file type")
+    try:
+        if not file.content_type.startswith("video/"):
+            raise HTTPException(status_code=400, detail="Invalid file type")
 
-    filename = f"{datetime.now().isoformat().replace(':', '-')}_{user.id}_{file.filename}"
-    file_path = os.path.join(UPLOAD_DIR, filename)
+        filename = f"{datetime.now().isoformat().replace(':', '-')}_{user.id}_{file.filename}"
+        file_path = os.path.join(UPLOAD_DIR, filename)
 
-    with open(file_path, "wb") as f:
-        content = await file.read()
-        f.write(content)
+        with open(file_path, "wb") as f:
+            content = await file.read()
+            f.write(content)
 
-    return {"filename": filename, "message": "Upload successful"}
+        supabase.table("video").insert({"user_id": user.id, "file_location": f"{PROJECT_PATH}\\videos\\{filename}"}).execute()
+
+        return {"filename": filename, "message": "Upload successful"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Upload failed: {str(e)}")
--- app/core/constants.py
+++ app/core/constants.py
@@ -1,1 +1,6 @@
+import os
+import sys
+
 UPLOAD_DIR = "videos"
+
+PROJECT_PATH = os.path.dirname(os.path.abspath(sys.argv[0]))
