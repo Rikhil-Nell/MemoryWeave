35674ee6665ca0e0b98ca1e9d9ee5898cdd1f354
Abhiram <abhiramvsa7@gmail.com>
2025-04-18 13:43:40 +0530

    voice agent working (backend)

--- src/voice.py
+++ src/voice.py
@@ -1,53 +1,44 @@
-import os
-import asyncio
-
-from livekit.agents import JobContext, WorkerOptions, cli, JobProcess
-from livekit.agents.llm import (
-    ChatContext,
-    ChatMessage,
-)
-from livekit.agents import AgentSession, Agent, RoomInputOptions
-from livekit.plugins import deepgram, silero, cartesia, openai
-
 from dotenv import load_dotenv
+from livekit import agents
+from livekit.agents import AgentSession, Agent, RoomInputOptions
+from livekit.plugins import (
+    openai,
+    cartesia,
+    deepgram,
+    silero,
+)
 
 load_dotenv()
 
 
-def prewarm(proc: JobProcess):
-    proc.userdata["vad"] = silero.VAD.load()
+class Assistant(Agent):
+    def __init__(self) -> None:
+        super().__init__(instructions="You are a helpful voice AI assistant.")
 
 
-async def entrypoint(ctx: JobContext):
-    initial_ctx = ChatContext(
-        messages=[
-            ChatMessage(
-                role="system",
-                content="You are a voice assistant. Pretend we're having a human conversation, no special formatting or headings, just natural speech.",
-            )
-        ]
-    )
-
-    assistant = AgentSession(
-        vad=ctx.proc.userdata["vad"],
-        stt=deepgram.STT(),
-        llm=openai.LLM(
-            base_url="https://api.cerebras.ai/v1",
-            api_key=os.environ.get("CEREBRAS_API_KEY"),
-            model="llama3.1-8b",
-        ),
-        tts=cartesia.TTS(voice="248be419-c632-4f23-adf1-5324ed7dbf1d"),
-        chat_ctx=initial_ctx,
-    )
-    
-    
-    
-
+async def entrypoint(ctx: agents.JobContext):
     await ctx.connect()
-    assistant.start(ctx.room)
-    await asyncio.sleep(1)
-    await assistant.say("Hi there, how are you doing today?", allow_interruptions=True)
+
+    session = AgentSession(
+        stt=deepgram.STT(model="nova-3", language="multi"),
+        llm=openai.LLM.with_cerebras(
+        model="llama-3.3-70b",
+        ),
+        tts=cartesia.TTS(),
+        vad=silero.VAD.load(),
+    )
+
+    await session.start(
+        room=ctx.room,
+        agent=Assistant(),
+        room_input_options=RoomInputOptions(
+        ),
+    )
+
+    await session.generate_reply(
+        instructions="Greet the user and offer your assistance."
+    )
 
 
 if __name__ == "__main__":
-    cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint, prewarm_fnc=prewarm))
+    agents.cli.run_app(agents.WorkerOptions(entrypoint_fnc=entrypoint))
